5b5f007ca01ee1ff3adcedb6e504b55c60e2cee2
unknown <pankznegi4638@gmail.com>
2023-01-28 20:46:13 +0530

    up to rotated binary search

--- learn-java/src/com/pankz/search/RotatedBinarysearch.java
+++ learn-java/src/com/pankz/search/RotatedBinarysearch.java
@@ -2,34 +2,69 @@ package com.pankz.search;
 
 public class RotatedBinarysearch {
     public static void main(String[] args) {
-        int[] array={4,5,6,7,0,1,2,3,};
-        int Pivot=getPivot(array);
-        System.out.println("pivot element is ="+ " "+Pivot);
+        int[] array = {4, 5, 6, 7, 0, 1, 2,3};
+        int Pivot = getPivot(array);
+        System.out.println("pivot element is =" + " " + Pivot);
+        int searched_element=search(array,0);
+        System.out.println("searched element is at index"+ " "+searched_element);
 
     }
-    static int getPivot(int[]arr)
+
+    static int search(int[] arr, int target)   //function for searching in rotated binary araay after getting pivot
     {
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end)
-        {
-            int mid=start+(end-start/2);
-            if(mid<end&& arr[mid]>arr[mid+1]) //possible answers
-            {
-                return arr[mid];
+        int pivot = getPivot(arr);
+        //if pivot is not found then array is not rotated
+        if (pivot == -1) {
+            return binarysearch(arr, target, 0, arr.length - 1);//then do normal binary search
+        }
+        //If pivot is found then do saerching in ASC order arrays
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+        if (target>= arr[0]) {
+            return binarysearch(arr, target, 0, pivot - 1);
+        } else {
+            return binarysearch(arr, target, pivot + 1, arr.length-1 );
+        }
+    }
+
+
+
+    static int binarysearch(int[] arr, int target, int start, int end) {
+
+        while (start <=end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > mid) {
+                start = mid + 1;
+            } else {
+                return mid;
             }
-            if(mid>start && arr[mid]<arr[mid-1])  //possible answers
+        }
+        return -1;
+    }
+
+    static int getPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start<=end) {
+            int mid = start + (end - start)/ 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) //possible answers
             {
-                return arr[mid-1];
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1])  //possible answers
+            {
+                return mid - 1;
 
             }
-            if(arr[mid]<=arr[start])   //checking conditions for first if above two cases fails
+            if (arr[mid] <= arr[start])   //checking conditions for first if above two cases fails
             {
-                end=mid-1;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else {
-                start=mid+1;
-            }
-        } return -1;
-    }
-}
+        }
+        return -1;
+    }}
