2f9c8476ef3802f40134aae4b2b514b0835ba960
unknown <pankznegi4638@gmail.com>
2023-01-30 18:57:38 +0530

    formatting & placeholders

---
+++ learn-java/src/com/pankz/Strings/Preetyprinting.java
@@ -0,0 +1,17 @@
+package com.pankz.Strings;
+
+public class Preetyprinting {
+    public static void main(String[] args) {
+        float a=123.76856f;
+        System.out.printf("formatted no. is%.2f",a);  //%.2f--->formatting up to - decimal point
+        System.out.println();
+        System.out.printf("formatted value of pi is %.3f",Math.PI); //--use printf not ln
+        System.out.println();
+        System.out.printf("Hello my name is %s and i am learning %s","Pankaj","Java");
+
+
+    }
+}
+
+//placeholders %s ,%.2f
+
