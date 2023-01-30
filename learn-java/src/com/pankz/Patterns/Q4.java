2c7c1dea53bfb09c54c687d7eda71eeca95b0c8e
unknown <pankznegi4638@gmail.com>
2023-01-30 21:29:55 +0530

    yet another pattern

---
+++ learn-java/src/com/pankz/Patterns/Q4.java
@@ -0,0 +1,20 @@
+package com.pankz.Patterns;
+
+public class Q4 {
+    public static void main(String[] args) {
+        pattern(5);
+
+    }
+    static void pattern(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
