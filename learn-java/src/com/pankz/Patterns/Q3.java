d8345ae1e95f54ee808969e9a84d3d872a4c521d
unknown <pankznegi4638@gmail.com>
2023-01-30 21:28:58 +0530

    pattern3

---
+++ learn-java/src/com/pankz/Patterns/Q3.java
@@ -0,0 +1,19 @@
+package com.pankz.Patterns;
+
+public class Q3 {
+    public static void main(String[] args) {
+        pattern(6);
+
+    }
+    static void pattern(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i+1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
