ca5bde45553b002bbaa4761ecf8a5535354195b3
unknown <pankznegi4638@gmail.com>
2023-02-01 12:49:20 +0530

    counting zeroes via recursion

---
+++ learn-java/src/com/pankz/Simplerecurionproblems/Countzeroes.java
@@ -0,0 +1,27 @@
+package com.pankz.Simplerecurionproblems;
+
+public class Countzeroes {
+    public static void main(String[] args) {
+       int ans= count(12040606);
+        System.out.println(ans);
+
+
+    }
+    static int count(int n) {
+        return helper(  n, 0);
+    }
+
+   private static int helper(int n,int c)
+    {
+        if(n==0)
+        {
+            return c;
+        }
+        int rem=n%10;
+        if(rem==0)
+        {
+            return helper(n/10,c+1);
+        }
+        return helper(n/10,c);
+    }
+}
