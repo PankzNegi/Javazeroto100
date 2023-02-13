f42b642dc5c8b71b35430853a235f3cf5316d0e6
unknown <pankznegi4638@gmail.com>
2023-02-13 21:04:41 +0530

    Special class that represent fixed set of constants

---
+++ learn-java/src/com/pankz/OOPS/Enums/Basic.java
@@ -0,0 +1,25 @@
+package com.pankz.OOPS.Enums;
+
+public class Basic {
+    enum Weekday
+    {
+        Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+        //enums constants
+        //by default they are Public,Static &final
+        //final prevents inheritence
+        //Type is classname
+
+    }
+
+    public static void main(String[] args) {
+//        Weekday week;
+//        week=Weekday.Monday;
+        for(Weekday day:Weekday.values())
+        {
+            System.out.println(day);
+        }
+    }
+}
+  //  Java Enum is a data type which contains fixed set of constants
+//An enum is a special "class" that represents a group of constants
+// (unchangeable variables, like final variables).
