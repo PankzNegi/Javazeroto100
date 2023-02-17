3ceabd7b7443385eb8ce6df7d2bffd258b8371fe
unknown <pankznegi4638@gmail.com>
2023-02-17 10:18:23 +0530

    Builder Design Pattern EX1

---
+++ learn-java/src/com/pankz/OOPS/builderdesignpattern/Vehicle.java
@@ -0,0 +1,46 @@
+package com.pankz.OOPS.builderdesignpattern;
+
+public class Vehicle {
+    private String engine;
+    private int wheel;
+    //optional parameter is airbag
+    private int airbag;
+    //Getters
+    public String getEngine() {
+        return engine;
+    }
+
+    public int getWheel() {
+        return wheel;
+    }
+
+    public int getAirbag() {
+        return airbag;
+    }//Private Constructor
+    private Vehicle(VehicleBuilder builder) {
+        this.engine = builder.engine;
+        this.wheel = builder.wheel;
+        this.airbag = builder.airbag;
+    }
+
+    public static class VehicleBuilder{
+        private String engine;
+        private int wheel;
+        private int airbag;
+
+        public VehicleBuilder(String engine,int wheel) {
+            this.engine = engine;
+            this.wheel=wheel;
+        }
+
+        public VehicleBuilder setAirbag(int airbag) {
+            this.airbag = airbag;
+            return this;
+
+        }
+        public  Vehicle build()
+        {
+            return new Vehicle(this);
+        }
+    }
+}
---
+++ learn-java/src/com/pankz/OOPS/builderdesignpattern/VehicleMain.java
@@ -0,0 +1,16 @@
+package com.pankz.OOPS.builderdesignpattern;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle car=new Vehicle.VehicleBuilder("200cc",4)
+                .setAirbag(4).build();
+        Vehicle bike=new Vehicle.VehicleBuilder("150cc",2).build();
+        System.out.println(car.getEngine());
+        System.out.println(car.getAirbag());
+        System.out.println(car.getWheel());
+
+        System.out.println(bike.getEngine());
+        System.out.println(bike.getWheel());
+        System.out.println(bike.getAirbag());
+    }
+}
