6c103389acd0a999562b6163938ed3d21e62e5fd
Pankz <pankznegi4638@gmail.com>
2023-03-06 21:18:48 +0530

    main is executed by thread

---
+++ learn-java/src/com/pankz/Concurrency/ThreadExample.java
@@ -0,0 +1,8 @@
+package com.pankz.Concurrency;
+
+public class ThreadExample {
+    public static void main(String[] args) { //Main is executed by thread when JVM starts application
+        Thread thread=new Thread();
+        thread.start();
+    }
+}
