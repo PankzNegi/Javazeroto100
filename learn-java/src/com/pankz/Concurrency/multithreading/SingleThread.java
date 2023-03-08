91a5fceae8f2ea99c3c04000011fe8ddea9e6407
Pankz <pankznegi4638@gmail.com>
2023-03-08 21:48:57 +0530

    sequential execution of thread as follows

---
+++ learn-java/src/com/pankz/Concurrency/multithreading/SingleThread.java
@@ -0,0 +1,21 @@
+package com.pankz.Concurrency.multithreading;
+
+public class SingleThread {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Learning java");
+            Thread.sleep(200);
+        }
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Learning HTML");
+            Thread.sleep(200);
+        }
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Learning OOPS");
+            Thread.sleep(200);
+        }
+    }
+}
